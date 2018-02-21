a87c6f24ae9a92c4bd24a2c88bec792c6a1d5cc2
Prachi <prachijulka31@gmail.com>
2018-02-21 16:21:49 +0530

    Q10

---
+++ hibernate-assignment/src/main/java/domains/Address.java
@@ -0,0 +1,32 @@
+package domains;
+
+public class Address {
+
+    private Integer streetNumber;
+    private String location;
+    private String state;
+
+    public Integer getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(Integer streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
