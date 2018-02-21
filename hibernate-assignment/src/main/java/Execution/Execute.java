f0691329d65b3afeb892ebb720e8f5e90e08f06c
Prachi <prachijulka31@gmail.com>
2018-02-21 15:37:33 +0530

    Q3

--- hibernate-assignment/src/main/java/Execution/Execute.java
+++ hibernate-assignment/src/main/java/Execution/Execute.java
@@ -20,22 +20,22 @@ public class Execute {
         SessionFactory sessionFactory=new Configuration().configure().buildSessionFactory();
         Session session=sessionFactory.openSession();
         session.beginTransaction();
-        /*create*/ //session.save(author);
-        Author author1=(Author)session.get(Author.class,1);
+        session.save(author);
+       /* Author author1=(Author)session.get(Author.class,1);
         author1.setAge(24);
-
+*/
         /*Update*///session.update(author1);
 
         //READ
-        System.out.println("READ OPERATION");
+       /* System.out.println("READ OPERATION");
         System.out.print(author1.getAuthorId()+" ");
         System.out.print(author1.getFirstName()+" ");
         System.out.print(author1.getLastName()+" ");
         System.out.println(author1.getAge());
-
+*/
         //DELETE
-        session.delete(author1);
-        session.getTransaction().commit();
+  /*      session.delete(author1);
+  */      session.getTransaction().commit();
         session.close();
         sessionFactory.close();
     }
